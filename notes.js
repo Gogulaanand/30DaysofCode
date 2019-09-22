606376c94e8e76f08920f6fae03c1e396bc6c9b0
Gogulaanand R <gogulaanand02@gmail.com>
2019-09-22 23:46:53 +0530

    Notes

---
+++ notes.js
@@ -0,0 +1,20 @@
+
+/*
+
+Notes section:
+
+imperative => a set of commands for the computer to perform a task
+
+first order function => functions that are passes into, returned from other functions or assigned to variables
+
+lambda function => a first order function
+
+higher order function => function that accepts or returns functions
+
+
+Principles of functional prog:
+    - declare funtion arguments and pass them when used to avoid confusion later on incase global avr are being accessed
+    - do ot alter global var, use local ones
+
+*/
+
